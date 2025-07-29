fb9c4bddc8b80b048c47b6c2c8f1c94f912b2014
Cristhoper86 <cristhopercriollo13@gmail.com>
2025-07-28 20:26:29 -0500

    Add files via upload

---
+++ Dibujar un triángulo con asteriscos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int altura;
+
+    cout << "Ingresa la altura del triángulo: ";
+    cin >> altura;
+
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl; // Salto de línea después de cada fila
+    }
+
+    return 0;
+}
